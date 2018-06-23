16b13bd8d8e244123c5a887790e8683d0e89a30c
Chip Stewart <donald.a.stewart@gmail.com>
2018-06-23 11:13:48 -0400

    matlab scripts and data files for comment on damage

---
+++ make_figures.m
@@ -0,0 +1,12 @@
+clear
+A='~/Downloads/matlab'
+cd(A)
+restoredefaultpath
+
+%rmpath	/Users/stewart/Projects/matlab /Users/stewart/Documents/MATLAB	 /Users/stewart/Projects/Cancer/tools/matlab
+%rmpath  /Users/stewart/CancerGenomeAnalysis/trunk/matlab /Users/stewart/CancerGenomeAnalysis/trunk/matlab/seq
+    
+figure(1)
+Figure1(A)
+figure(2)
+Figure2(A)
